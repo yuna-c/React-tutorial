97b2c3c8ffab161c9738cbb2e7b5bb940335fa61
yuna <yuna744@gmail.com>
2023-11-20 10:58:48 +0900

    1120 컴포넌트 설명

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+	return (
+		<div className='App'>
+			<h1>React</h1>
+		</div>
+	);
+}
+
+export default App;
+
+/*
+컴포넌트 함수의 필수 조건
+1. 함수명을 대문자로 시작
+2. JSX를 리턴
+
+JSX: 리액트에서 전용으로 쓰이는 돔 생성문법
+원래는 react.createElement()라는 메서드를 이용해서 복잡하게 구성해야 하는 로직을 Html태그를 따라한 친화적 문법으로 편하게 Dom 생성을 위한 React만의 문법 체계
+*/
