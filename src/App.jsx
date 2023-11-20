2b39268c3afc787d904b4d96505d545123c63108
yuna <yuna744@gmail.com>
2023-11-20 12:19:43 +0900

    1120  컴포넌트 생성시 세부 규칙 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Title from './Title'; //상대경로
+//상대경로
 
 function App() {
 	return (
-		<div className='App'>
-			<Title />
-		</div>
+		<>
+			<div className='App'></div>
+			<div className='box'>sdfsfsdf</div>
+		</>
 	);
 }
 
@@ -14,6 +15,9 @@ export default App;
 컴포넌트 함수의 필수 조건
 1. 함수명을 대문자로 시작
 2. JSX를 리턴
+3. 해당 컴포넌트 함수는 복수개의 JSX요소를 리턴 불가능 
+4. 복수개의 요소를 내보내고 싶을 때는 상위요소로 wrapping 처리 해서 내보냄 
+5. 불필요한 상위 wrapping 태그 출력 싫을 시 fragment로 <></> 출력
 
 JSX: 리액트에서 전용으로 쓰이는 돔 생성문법
 원래는 react.createElement()라는 메서드를 이용해서 복잡하게 구성해야 하는 로직을 Html태그를 따라한 친화적 문법으로 편하게 Dom 생성을 위한 React만의 문법 체계
--- src/Btn.jsx
+++
@@ -1,3 +0,0 @@
-export default function Btn() {
-	return <div>Btn</div>;
-}
