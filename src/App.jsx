14959789f909c7e0755a7d57a1487678501dcac9
yuna <yuna744@gmail.com>
2023-11-21 10:07:31 +0900

    1121  JSX에서 이벤트 핸들러 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 export default function App() {
-	const person = '홍길동';
+	const name = '홍길동';
 	const cl_name = 'wrap';
+	const changebutton = (e, txt) => {
+		e.target.innerText = txt;
+		// console.log(txt);
+	};
 
-	/* 
-	JSX문 안쪽에 {}안에서 가능한 연산이 제한적
-	1. 변수 치환
-	2. 조건문(삼항연산자 ,&&연산자)
-	3. 반복문(map)
-	*/
+	//미션 : 2번 txt에 문자를 집어 넣어서 button글자 내용을 변경되게 호출
 	return (
 		<>
 			<section className={cl_name}>
-				<h1>안녕하세요. 제 이름은 {person} 입니다.</h1>
+				<h1>안녕하세요 제이름은 {name} 입니다.</h1>
+				<button onClick={(e) => changebutton(e, '버튼2')}>버튼</button>
 			</section>
 		</>
 	);
