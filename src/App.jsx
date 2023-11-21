563c36d3f05526a14b8b7aaf5309f5cf2a7c2a7b
yuna <yuna744@gmail.com>
2023-11-21 12:53:30 +0900

    1121 prop값 전달 받을 때 보통 비구조화할당으로 각 property값 뽑아서 전달 받음

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Card from './card/Card';
 
 export default function App() {
 	const colors = ['pink', 'lightblue', 'lightcoral', 'lightcyan', 'lightgreen'];
+	const widths = ['200px', '240px', '230px', '100px', '50px'];
 
 	return (
 		<>
@@ -9,8 +10,9 @@ export default function App() {
 			{colors.map((color, idx) => {
 				// 컴포넌트 호출시 특정 값을 key={value} 형태로 컴포넌트 내부에 전달 가능 {props}
 				// 컨트롤 파일명 클릭 바로 파일 열어줌
-				return <Card key={color + idx} colorName={color} index={idx} />;
+				return <Card key={color + idx} name={color} index={idx} width={250} show={'block'} />;
 			})}
+			<Card key={6} name={'black'} index={6} width={100} show={'none'} />
 		</>
 	);
 }
--- src/card/Card.jsx
+++ src/card/Card.jsx
@@ -1,13 +1,22 @@
 import './Card.scss';
-export default function Card(props) {
+
+function Card(name, index, width, show) {
 	// 부모 컴퍼넌트에 전달되는 요소들을 props라는 객체로 묶어서 자식 컴포넌트의 파라미터로 전달 받음
-	console.log(props);
+	// props{ name:'색상', index:'순서', width:'넓이', show:'display속성값' }
+	// const { name, index, width, show } = props;
+	// 만약 이전처럼 단지 props라는 파라미터 이름으로만 객체를 통으로 전달하면 props로 전달되는 값이 많을 떄 어떤 값이 전달되는지 파악하기 힘들. 그러므로 보통 비구조화할당을 이용해서 직접 전달{일반적)
+	console.log(name, index, width, show);
+
+	// 미션1 - 부모 해당 컴포넌트 호출시 props로 부터 width값을 전달 받아서 각 카드 컴포넌트 크기가 다르도록 호출
+	// 미션2 - 부모 특정 값에 따라 txtBox의 보임유무를 결정할 수 있게 호출
+
 	return (
-		<article className='card'>
-			<div className='bgBox' style={{ backgroundColor: props.colorName }}></div>
-			<div className='txtBox'>
-				{props.index + 1} -{props.colorName}
+		<article className='card' style={{ width }}>
+			<div className='bgBox' style={{ backgroundColor: name }}></div>
+			<div className='txtBox' style={{ display: show }}>
+				{index + 1} - {name}
 			</div>
 		</article>
 	);
 }
+export default Card;
