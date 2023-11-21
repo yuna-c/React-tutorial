9fc6133413f9c916991b62b45c37219ccdd252b3
yuna <yuna744@gmail.com>
2023-11-21 12:14:25 +0900

    1121 ctrl + file click

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
 			<h1>Color Chart</h1>
 			{colors.map((color, idx) => {
 				// 컴포넌트 호출시 특정 값을 key={value} 형태로 컴포넌트 내부에 전달 가능 {props}
+				// 컨트롤 파일명 클릭 바로 파일 열어줌
 				return <Card key={color + idx} colorName={color} index={idx} />;
 			})}
 		</>
