2c4ad72f2686bf216a3113fad6eda66c70e91d54
yuna <yuna744@gmail.com>
2023-11-21 11:52:05 +0900

    1121 map으로 특정 데이터를 반복 돌면서 동적으로 JSX반환시 key값이 필요한 이유

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import Box from './box/Box';
-
 export default function App() {
+	const colors = ['orange', 'red', 'green', 'blue', 'pink'];
 	return (
 		<>
-			<section>
-				<Box />
-				<Box />
-				<Box />
-			</section>
+			<h1>Color Chart</h1>
+
+			<ul>
+				{/* <li style={{ backgroundColor: '#000' }}></li>
+				<li style={{ backgroundColor: '#111' }}></li>
+				<li style={{ backgroundColor: '#222' }}></li>
+				<li style={{ backgroundColor: '#333' }}></li>
+				<li style={{ backgroundColor: '#444' }}></li> */}
+				{colors.map((color, idx) => {
+					return (
+						//return문에서 map을 통해 반복적으로 생성되는 가상돔 요소에는 무조건 고유의 key값을 줘야함
+						//리액트로 하여금 여러개의 반복 요소 중에서 수정 요소를 효율적으로 비교 탐색 하기 위함
+						//key값 고유문자 값 만들기 color + idx/ performance().now+idx/
+						// DB의 각 데이터에 내장되어 있는 Object_id를 활용하거나 실제 데이터와 순번을 결합해서 고유문자값 생성
+						<li key={color + idx} style={{ color: color }}>
+							{color}
+						</li>
+					);
+				})}
+			</ul>
 		</>
 	);
 }
