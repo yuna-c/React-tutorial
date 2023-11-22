a31a8ca1f58427beb168f95a27fe55a424ad55bf
yuna <yuna744@gmail.com>
2023-11-22 13:18:11 +0900

    1122 state 모션 관련 주의사항 2가지

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import './App.scss';
 
+/*
+	state관련 주의사항
+	1. state값 변경시 해당값을 무조건 다음 렌더링 사이클에 반영됨
+	2. 위의 특성에 기인하여 특정 숫자값을 증감시킬 때 후위 증감연산자 사용 지양
+*/
+
 export default function App() {
-	// state값 변경 순서에 따라 결과 화면 상이함
 	console.log('render');
 	const [Index, setIndex] = useState(0);
-
 	return (
 		<>
 			{/* 물리적인 적용 방법 */}
