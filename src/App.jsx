723223f6c04f9251449e6cad9ccbdbcd7953b74a
yuna <yuna744@gmail.com>
2023-11-23 12:11:01 +0900

    1123 useRef 핵심정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useRef } from 'react';
 import './App.scss';
 
+/*
+useRef에서 알아야 하는 핵심정리
+1. DOM 참조 객체에 담는 법
+1-1. const 변수명 = useRef(null); : 돔을 담은 빈 참조 객체 생성
+1-2. <div ref={변수명}></div> : 원하는 요소에 참조 객체 연결
+1-3. 변수명.current.style~ : 참조 객체에 담겨있는 돔 호출
+
+2. 모션 작업시 화면 재랜더링 하지 않으면서 수치값 변경하는 방법
+2-1. const num = useRef(초기수치); 
+2-2. num.current = 변경할 수치값
+*/
+
 export default function App() {
 	console.log('render');
-	const num = useRef(0); //객체 : 참조객체를 만들어 줌
-	const refBox = useRef(null);
+	const num = useRef(0); //쓰임1 객체 : 참조객체를 만들어 줌(자주 바뀌는 수칙값)
+	const refBox = useRef(null); //쓰임2
 	const deg = 45;
 
 	console.log(num);
+	console.log(refBox);
 
 	// const minus = () => {
 	// 	num.current--;
@@ -21,7 +34,7 @@ export default function App() {
 	// };
 
 	const rotation = (count) => {
-		refBox.current.style.transform = `rotate(${count * 45}deg)`;
+		refBox.current.style.transform = `rotate(${count * deg}deg)`; // 쓰임 2 리얼돔 요소를 객체로 가져와서 바꿔야 할때
 	};
 	return (
 		<>
@@ -49,7 +62,7 @@ useRef :
 -- 쓰임 2에 대한 해설
 : 리액트에서 어쩔 수 없이 realDom을 직접 가져와서 이벤트 연결 해야 하는 경우 
 : 스크롤 모션 이벤트, 인풋요소 포커스 연결 이벤트
-: document.querySelector를 리액트의 제어에서 벗어난 이전 렌더링 사이클에서 생성된 과거 신뢰할 수 없는 Dom을 참조
+: document.querySelector를 리액트의 제어에서 벗어난 이전 랜더링 사이클에서 생성된 과거 신뢰할 수 없는 Dom을 참조
 : useRef를 통한 참조객체에 담겨있는 realDom은 React에서 관리하는 신뢰할 수 있는 최신 상태의 리얼돔을 참조
-: 참조객체current.querySelector 가능(최신돔) but! document객체 (이미 리엑트 생성전에 만들어진 바디 요소니까 신뢰성이 떨어짐)
+: 참조객체current.querySelector 가능(최신돔) but! document객체 (이미 리액트 생성전에 만들어진 바디 요소니까 신뢰성이 떨어짐)
 */
