27cb92db7ec89c357f0678f7dee299d0a55d7297
yuna <yuna744@gmail.com>
2023-11-23 09:39:05 +0900

    1123 Modal2 scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from 'react';
-import './App.scss';
-
-/*
-	state관련 주의사항
-	1. state값 변경시 해당값을 무조건 다음 렌더링 사이클에 반영됨
-	2. 위의 특성에 기인하여 특정 숫자값을 증감시킬 때 후위 증감연산자 사용 지양
-*/
+import Modal from './modal2/Modal';
 
 export default function App() {
-	console.log('render');
-	const [Index, setIndex] = useState(0);
 	return (
 		<>
-			{/* 물리적인 적용 방법 */}
-			<button onClick={() => setIndex(Index - 1)}>왼쪽으로 회전</button>
-			<button onClick={() => setIndex(Index + 1)}>오른쪽으로 회전</button>
-			<div className='box' style={{ transform: `rotate(${45 * Index}deg)` }}></div>
+			<h1>parent</h1>
+			<button>open</button>
+			<Modal />
 		</>
 	);
 }
---
+++ src/modal2/Modal.jsx
@@ -0,0 +1,10 @@
+import './Modal.scss';
+
+export default function Modal() {
+	return (
+		<div className='modal'>
+			<h1>Modal</h1>
+			<button>close</button>
+		</div>
+	);
+}
